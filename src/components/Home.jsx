b8fa9e6757c0cead29265fd1f07ad5d23066ea7b
RaveBlue <my-raveblue21@yahoo.com>
2026-03-09 22:03:11 -0600

    Add Link import to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 import GreenTea from "../images/GreenTea.png";
 import Strawberry from "../images/Strawberry.png";
